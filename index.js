890dfef957f1f5192450102c70318d778d788522
Rossi-Kellss <rossi.codingspace@gmail.com>
2023-07-20 10:07:20 +0545

    h1

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-gsap.to("#box",{
-    x:700
+gsap.from(".box",{
+    x:700,
+    duration:5,
+    delay:1,
+    backgroundColor:"green",
+    scale:0.5,
+    stagger:1
+
+})
+
+// gsap.from("#box",{
+//     x:0,
+//     duration:5,
+//     delay:1,
+//     backgroundColor:"blue",
+//     // scale:0.5
+
+// })
+
+gsap.to('h1',{
+    color:yellow,
+    x:400
+
+
 })
